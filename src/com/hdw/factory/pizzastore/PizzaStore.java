c3c4bb7e6bd515321dbff2644a8cd9b8ee190912
yidaoxian <yidaoxian@git.com>
2019-12-26 00:53:42 +0800

    factory pattern

--- src/com/hdw/factory/pizzastore/PizzaStore.java
+++ src/com/hdw/factory/pizzastore/PizzaStore.java
@@ -1,18 +1,22 @@
 package com.hdw.factory.pizzastore;
 
+import com.hdw.factory.pizzastore.absfactory.LdFactory;
+import com.hdw.factory.pizzastore.absfactory.NyFactory;
 import com.hdw.factory.pizzastore.factoryMethod.LDOrderPizza;
 import com.hdw.factory.pizzastore.factoryMethod.NYOrderPizza;
+import com.hdw.factory.pizzastore.simplefactory.SimplePizzaFactory;
 
 public class PizzaStore {
 
     public static void main(String[] args) {
         OrderPizza orderPizza1 = new OrderPizza();
 
-        com.hdw.factory.pizzastore.simplefactory.OrderPizza orderPizza2 = new com.hdw.factory.pizzastore.simplefactory.OrderPizza();
+        com.hdw.factory.pizzastore.simplefactory.OrderPizza orderPizza2 = new com.hdw.factory.pizzastore.simplefactory.OrderPizza(new SimplePizzaFactory());
 
         com.hdw.factory.pizzastore.factoryMethod.OrderPizza orderPizza3 = new LDOrderPizza();
         //com.hdw.factory.pizzastore.factoryMethod.OrderPizza orderPizza3 = new NYOrderPizza();
 
-        com.hdw.factory.pizzastore.absfactory.OrderPizza orderPizza4 = new com.hdw.factory.pizzastore.absfactory.OrderPizza();
+        com.hdw.factory.pizzastore.absfactory.OrderPizza orderPizza4 = new com.hdw.factory.pizzastore.absfactory.OrderPizza(new LdFactory());
+        //com.hdw.factory.pizzastore.absfactory.OrderPizza orderPizza4 = new com.hdw.factory.pizzastore.absfactory.OrderPizza(new NyFactory());
     }
 }
